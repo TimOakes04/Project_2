43444ed0798fc772b8ada09fc160148cfc711f39
TimOakes04 <toakes@csumb.edu>
2025-10-24 16:19:40 -0700

    fixed main on Database

--- app/src/main/java/com/example/project_2/MainActivity.java
+++ app/src/main/java/com/example/project_2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.project_2;
 
+import static android.os.Build.VERSION_CODES_FULL.R;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -14,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
