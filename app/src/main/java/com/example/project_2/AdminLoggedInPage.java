7d2533c823da5791a6c9d53c362271237312a5e8
Peter <petergloag@gmail.com>
2025-12-05 13:01:16 -0800

    Added functionality for mainPageButtonAdmin in activity_admin_logged_in_page. Added activityAdminLoggedInPageIntentFactory in AdminLoggedInPage.java

--- app/src/main/java/com/example/project_2/AdminLoggedInPage.java
+++ app/src/main/java/com/example/project_2/AdminLoggedInPage.java
@@ -1,6 +1,9 @@
 package com.example.project_2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AdminLoggedInPage extends AppCompatActivity {
+import com.example.project_2.databinding.ActivityAdminLoggedInPageBinding;
 
+public class AdminLoggedInPage extends AppCompatActivity {
+    ActivityAdminLoggedInPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_logged_in_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityAdminLoggedInPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.mainPageButtonAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(MainPage1.mainPage1IntentFactory(getApplicationContext()));
+            }
         });
+    }
+
+    public static Intent activityAdminLoggedInPageIntentFactory(Context context){
+        return new Intent(context, AdminLoggedInPage.class);
     }
 }
