d2ba4a9fe7c8e4f8d407fc97ecd36d9d6eeb3eed
TimOakes04 <toakes@csumb.edu>
2025-12-06 12:00:38 -0800

    Fixing stuff

--- app/src/main/java/com/example/project_2/MainActivity.java
+++ app/src/main/java/com/example/project_2/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.project_2;
 
-import static android.os.Build.VERSION_CODES_FULL.R;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.project_2.databinding.ActivityLoginPageBinding;
 
